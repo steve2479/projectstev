a8b31d0b2b7d150cb5178181f18ce86cecc15985
arnoldwamae <arnoldwamae2@gmail.com>
2021-08-08 14:35:33 +0300

    Edited Dbconnnect

--- admin/admin_pic.php
+++ admin/admin_pic.php
@@ -1,25 +1,32 @@
  <?php
- include('./lib/dbcon.php'); 
-dbcon(); 
- include('session.php');
- 
- 
-                            if (isset($_POST['change'])) {
-                               
+    include('./lib/dbcon.php');
+    include("../app/DotEnv.php");
+    (new DotEnv('../.env'))->load();
+    $host = getenv('DB_HOST');
+    $user = getenv('DB_USERNAME');
+    $pass = getenv('DB_PASSWORD');
+    $db  = getenv('DB_NAME');
+    dbcon($host, $user, $pass, $db);
 
-                                $image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
-                                $image_name = addslashes($_FILES['image']['name']);
-                                $image_size = getimagesize($_FILES['image']['tmp_name']);
+    include('session.php');
 
-                                move_uploaded_file($_FILES["image"]["tmp_name"], "uploads/" . $_FILES["image"]["name"]);
-                                $adminthumbnails = "uploads/" . $_FILES["image"]["name"];
-								
-								mysqli_query($conn,"update admin set adminthumbnails = '$adminthumbnails' where admin_id  = '$session_id' ")or die(mysqli_error());
-								
-								?>
- 
-								<script>
-								window.location = "dashboard.php";  
-								</script>
 
-                       <?php     }  ?>
+    if (isset($_POST['change'])) {
+
+
+        $image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
+        $image_name = addslashes($_FILES['image']['name']);
+        $image_size = getimagesize($_FILES['image']['tmp_name']);
+
+        move_uploaded_file($_FILES["image"]["tmp_name"], "uploads/" . $_FILES["image"]["name"]);
+        $adminthumbnails = "uploads/" . $_FILES["image"]["name"];
+
+        mysqli_query($conn, "update admin set adminthumbnails = '$adminthumbnails' where admin_id  = '$session_id' ") or die(mysqli_error());
+
+    ?>
+
+     <script>
+         window.location = "dashboard.php";
+     </script>
+
+ <?php     }  ?>
